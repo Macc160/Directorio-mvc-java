aed7ccf46d847067eb2d577786ff0e9617bf9385
miltonUnicamacho <miltonadolofcoretescortes@gamail.com>
2025-11-17 22:17:26 -0500

    "feat(controlador): Implementa el Controlador y la clase App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import modelo.BaseDeDatos;
+import vista.DirectorioVista;
+import controlador.ControladorDirectorio;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        BaseDeDatos modelo = new BaseDeDatos();
+        DirectorioVista vista = new DirectorioVista();
+        ControladorDirectorio controlador = new ControladorDirectorio(modelo, vista);
+
+        controlador.iniciar();
     }
 }
